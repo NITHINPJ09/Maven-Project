55467df0e8542669373b45c324671608aac331b8
Nithin Prince John <nithinpj09@gmail.com>
2023-03-26 22:21:44 +0530

    Update App.java

--- src/main/java/com/nithin/app/App.java
+++ src/main/java/com/nithin/app/App.java
@@ -1,21 +1,20 @@
 package com.nithin.app;
-
 import java.util.Scanner;  
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Welcome to DevOps!" );
-        App app = new App();
-        int x, y, sum;  
+        System.out.println("PROGRAM FOR ADDING TWO NUMBERS");
+        App obj = new App();
+        int x, y, result;  
         Scanner sc = new Scanner(System.in);  
         System.out.print("Enter the first number: ");  
         x = sc.nextInt();  
         System.out.print("Enter the second number: ");  
         y = sc.nextInt();   
-        sum = app.sum(x, y);
-        System.out.println( "Sum : " + sum);
+        result = app.sum(x, y);
+        System.out.println("Sum : " + result);
     }
 
     public int sum(int a, int b)
