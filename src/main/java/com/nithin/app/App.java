790980c91d4c287e67d74200283fca8c5a2ed9c1
Nithin Prince John <nithinpj09@gmail.com>
2023-03-27 17:09:27 +0530

    Update App.java

--- src/main/java/com/nithin/app/App.java
+++ src/main/java/com/nithin/app/App.java
@@ -1,5 +1,7 @@
 package com.nithin.app;
-import java.util.Scanner;  
+import java.util.Scanner; 
+// import Cowsay class from the library
+import com.github.ricksbrown.cowsay.Cowsay; 
 
 public class App 
 {
@@ -14,7 +16,9 @@ public class App
         System.out.print("Enter the second number: ");  
         y = sc.nextInt();   
         result = obj.sum(x, y);
-        System.out.println("Sum : " + result);
+        String[] cowArgs = new String[]{String.valueOf(result)};
+        String cowString = Cowsay.say(cowArgs);
+	    System.out.println(cowString);
     }
 
     public int sum(int a, int b)
