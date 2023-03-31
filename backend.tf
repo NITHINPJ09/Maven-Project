b348de976f5b5ff1bdf41d58773aa821e972c1c9
Nithin Prince John <nithinpj09@gmail.com>
2023-03-31 14:04:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "mavenproject100sa2023"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
-    access_key           = "tkWvKbDxpY3TIyycbb8xEoPVDrQvQmwVtWsEw0YNd19UxIoAIJNj3UlQHcCCA+YD8AI/mhZqWqCv+AStkehJEQ="
+    access_key           = "tkWvKbDxpY3TIyycbb8xEoPVDrQvQmwVtWsEw0YNd19UxIoAIJNj3UlQHcCCA+YD8AI/mhZqWqCv+AStkehJEQ=="
   }
 }
