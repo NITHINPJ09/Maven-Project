8ec618944d7c2d7cf4b6d3382ec956fdb4667e2c
Nithin Prince John <nithinpj09@gmail.com>
2023-03-31 14:34:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,16 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "AI-102"
+    storage_account_name = "mavenproject100sa2023"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+    access_key           = var.ASAK
+  }
+}
+
 locals {
   resource_group="app-grp"
   location="East US"
