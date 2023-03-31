52b5f63e9959607e738b553d96cdf341bb660426
Nithin Prince John <nithinpj09@gmail.com>
2023-03-31 14:35:10 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "AI-102"
-    storage_account_name = "mavenproject100sa2023"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-    access_key           = var.ASAK
-  }
-}
