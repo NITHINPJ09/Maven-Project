ddfe2fe4260fe2695b3029595875a72f78bdf661
Nithin Prince John <nithinpj09@gmail.com>
2023-03-31 13:43:59 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "AI-102"
+    storage_account_name = "mavenproject100sa2023"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+    access_key           = "tkWvKbDxpY3TIyycbb8xEoPVDrQvQmwVtWsEw0YNd19UxIoAIJNj3UlQHcCCA+YD8AI/mhZqWqCv+AStkehJEQ=="
+  }
+}
