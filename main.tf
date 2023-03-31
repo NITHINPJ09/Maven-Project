31e976a93ed66c03f34f6250afc997fc3b5b91fd
Nithin Prince John <nithinpj09@gmail.com>
2023-03-31 12:14:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ terraform {
   }
 }
 
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "ASI" {}
+variable "ACI" {}
+variable "ACS" {}
+variable "ATI" {}
 
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = var.ASI
+  client_id       = var.ACI
+  client_secret   = var.ACS
+  tenant_id       = var.ATI
   features {}
 }
 
