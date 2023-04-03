2cfccaf3f16fb47753eda4a4989c87cb256863a4
Nithin Prince John <nithinpj09@gmail.com>
2023-04-03 10:02:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,26 @@ terraform {
   }
 }
 
-variable "ASI" {}
-variable "ACI" {}
-variable "ACS" {}
-variable "ATI" {}
+variable "ASI" { 
+  type        = string
+  sensitive   = true
+  description = "Subscription ID of Azure"
+}
+variable "ACI" {
+  type        = string
+  sensitive   = true
+  description = "Client ID"
+}
+variable "ACS" {
+  type        = string
+  sensitive   = true
+  description = "Client Secret"
+}
+variable "ATI" {
+  type        = string
+  sensitive   = true
+  description = "Tenant ID"
+}
 
 provider "azurerm" {
   subscription_id = var.ASI
