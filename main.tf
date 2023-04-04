11e2243c6a6d05c8c96cd7e88936a752767a6fa5
Nithin Prince John <nithinpj09@gmail.com>
2023-04-04 14:44:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,41 +65,34 @@ resource "azurerm_resource_group" "app_grp"{
 
 resource "azurerm_virtual_network" "app_network" {
   name                = "app-network"
-  location            = local.location
+  location            = azurerm_resource_group.app_grp.location
   resource_group_name = azurerm_resource_group.app_grp.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "SubnetA" {
   name                 = "SubnetA"
-  resource_group_name  = local.resource_group
+  resource_group_name  = azurerm_resource_group.app_grp.name
   virtual_network_name = azurerm_virtual_network.app_network.name
   address_prefixes     = ["10.0.1.0/24"]
-  depends_on = [
-    azurerm_virtual_network.app_network
-  ]
 }
 
 resource "azurerm_network_interface" "app_interface" {
   name                = "app-interface"
-  location            = local.location
-  resource_group_name = local.resource_group
+  location            = azurerm_resource_group.app_grp.location
+  resource_group_name = azurerm_resource_group.app_grp.name
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.SubnetA.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.app_public_ip.id
   }
-  depends_on = [
-    azurerm_virtual_network.app_network,
-    azurerm_public_ip.app_public_ip
-  ]
 }
 
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                = "linuxvm"
-  resource_group_name = local.resource_group
-  location            = local.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  location            = azurerm_resource_group.app_grp.location
   size                = "Standard_D2s_v3"
   admin_username      = "linuxusr"  
   network_interface_ids = [
@@ -119,41 +112,27 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
-  depends_on = [
-    azurerm_network_interface.app_interface,
-    tls_private_key.linux_key
-  ]
 }
 
 resource "azurerm_public_ip" "app_public_ip" {
   name                = "app-public-ip"
-  resource_group_name = local.resource_group
-  location            = local.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  location            = azurerm_resource_group.app_grp.location
   allocation_method   = "Static"
-  depends_on = [
-    azurerm_resource_group.app_grp
-  ]
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = "privatekeystore10090"
-  resource_group_name      = local.resource_group
-  location                 = local.location
+  resource_group_name      = azurerm_resource_group.app_grp.name
+  location                 = azurerm_resource_group.app_grp.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  depends_on = [
-    azurerm_resource_group.app_grp
-  ]
 }
 
 resource "azurerm_storage_container" "credentials" {
   name                  = "credentials"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
-  depends_on = [
-    azurerm_resource_group.app_grp,
-    azurerm_storage_account.storage_account
-  ]
 }
 
 resource "azurerm_storage_blob" "key" {
@@ -163,9 +142,6 @@ resource "azurerm_storage_blob" "key" {
   type                   = "Block"
   source                 = "linuxkey.pem"
   depends_on = [
-    azurerm_resource_group.app_grp,
-    azurerm_storage_account.storage_account,
-    azurerm_storage_container.credentials,
     azurerm_linux_virtual_machine.linux_vm
   ]
 }
