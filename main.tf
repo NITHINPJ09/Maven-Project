2b6dfb4e5ceed6ebbf8353a2defe16063e4722a3
Nithin Prince John <nithinpj09@gmail.com>
2023-04-10 09:57:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,23 @@ resource "azurerm_subnet" "SubnetA" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.app_grp.location
+  resource_group_name = azurerm_resource_group.app_grp.name
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "app_interface" {
   name                = var.network_interface_name
   location            = azurerm_resource_group.app_grp.location
@@ -39,20 +56,26 @@ resource "azurerm_network_interface" "app_interface" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "ni_sg_association" {
+  network_interface_id      = azurerm_network_interface.app_interface.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_linux_virtual_machine" "linux_vm" {
-  name                = var.virtual_machine_name
-  resource_group_name = azurerm_resource_group.app_grp.name
-  location            = azurerm_resource_group.app_grp.location
-  size                = "Standard_D2s_v3"
-  admin_username      = var.username
-  network_interface_ids = [
-    azurerm_network_interface.app_interface.id,
-  ]
+  name                            = var.virtual_machine_name
+  resource_group_name             = azurerm_resource_group.app_grp.name
+  location                        = azurerm_resource_group.app_grp.location
+  size                            = "Standard_D2s_v3"
+  computer_name                   = "myvm"
+  admin_username                  = var.username
+  disable_password_authentication = true
+  network_interface_ids           = [azurerm_network_interface.app_interface.id]
   admin_ssh_key {
     username   = var.username
     public_key = tls_private_key.linux_key.public_key_openssh
   }
   os_disk {
+    name                 = "myOsDisk"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
