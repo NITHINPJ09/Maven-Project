0fc70d0536ea7dd7e0bc132e5a2741f43b5fb3e0
Nithin Prince John <nithinpj09@gmail.com>
2023-04-10 13:09:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "AI-102"
-    storage_account_name = "mavenprojectsa2023"
+    storage_account_name = "mavenprojectstate2023"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
   }
